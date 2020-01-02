b18891b90b2f9ff63e19878f1d74f7e03128ab76
unknown <wldud8229@naver.com>
2020-01-02 10:54:00 +0900

    Add *.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,43 @@
 package day02;
 
-public class Circle {
+import java.util.Scanner;
 
-}
+public class Circle {
+    private double x, y;
+    private int radius;
+
+    public Circle(double x, double y, int radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double area() {
+        return radius * radius * 3.14;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        Circle c[] = new Circle[3];
+        double max;
+        int j = 0;
+        for (int i = 0; i < c.length; i++) {
+            System.out.print("x, y, radius==>");
+            Double x = s.nextDouble();
+            Double y = s.nextDouble();
+            int radius = s.nextInt();
+
+            c[i] = new Circle(x, y, radius);
+        }
+        max = c[0].area();
+
+        for (int i = 0; i < c.length; i++) {
+            if (max < c[i].area()) {
+                max = c[i].area();
+                j = i;
+            }
+        }
+        System.out.println("가장 큰 면적인 큰 원은" + "(" + c[j].x + "," + c[j].y + ")" + c[j].radius);
+    }
+} 
